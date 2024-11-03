2020aea8af5a9d55ae76864eb81c5d28c557c7e9
jttribiana <179311210+jttribiana@users.noreply.github.com>
2024-11-03 13:29:16 +0800

    Finish 1 and 2 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
 
+
+const repeatString = function (str, times) {
+    let result = ''
+    for (let i = 0; i < times; i++) {
+        result += str;
+    }
+    if (times < 0) {
+        return `ERROR`;
+    } else {
+        return result
+    }
+    
 };
+
+
+//parameters include the string to be repeated and the number to number of times the string will be repeated
+//usign array to give the words to be repeated and the repeat times to repeat the word
 
 // Do not edit below this line
 module.exports = repeatString;
