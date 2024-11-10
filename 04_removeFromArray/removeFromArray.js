b552e13661135e1dee17f88f24cde8c0e1fb2ea6
jttribiana <179311210+jttribiana@users.noreply.github.com>
2024-11-10 18:40:25 +0800

    Finish 5th exercise after two days - 3 hrs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...itemsToRemove){
+    for (let i = array.length - 1; i >= 0; i--){
+        let currentElement = array[i];
+        if (itemsToRemove.includes(currentElement)){
+            array.splice(i, 1);
+        };
+    }
+    return array
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(a, b) {
+    let finalSum = 0;
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    
+    for (let i = min; i <= max; i++) {
+        finalSum += i;
+} 
+    if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number" 
+        || !Number.isInteger(a) || !Number.isInteger(b)
+    ){
+        return `ERROR`  
+    } else return finalSum
+}
 // Do not edit below this line
 module.exports = sumAll;
