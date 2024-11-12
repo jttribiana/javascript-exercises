097509faed6571c3e4b1b0ebaebe3cce9b7d02ba
jttribiana <179311210+jttribiana@users.noreply.github.com>
2024-11-12 19:22:24 +0800

    Finish the 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+    let cel = (far - 32) * 5/9;
+    let celRounded = Number(cel.toFixed (1));
+    return celRounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celValue) {
+  let farValue = (9/5) * celValue + 32;
+    let farRounded = Number(farValue.toFixed (1));
+    return farRounded
 };
 
 // Do not edit below this line
