50a3cdc13fa6740d3b68653c82a59f64b48513e0
jttribiana <179311210+jttribiana@users.noreply.github.com>
2024-11-29 14:46:49 +0800

    Finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let rev = ''; 
+
+    let punctuation = /[\.,?!]/g;
+    let newStr = str.replace (punctuation, '');
+    let lowerCaseStr = newStr.toLowerCase();
+    let noWhiteSpace = lowerCaseStr.split(' ').join('');
+
+    for (let i = noWhiteSpace.length - 1; i >=0; i--){
+        rev += noWhiteSpace[i];
+    }
+    if (rev == noWhiteSpace){
+        return true
+    } else {
+        return false;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
