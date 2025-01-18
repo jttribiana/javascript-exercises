a263e99597eb4e99c225223049c2ce14f53f59c3
jttribiana <179311210+jttribiana@users.noreply.github.com>
2025-01-18 19:00:56 +0800

    Finish javascript exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n){
+        if (n == 0){
+                return 0;
+        } else if (n == 1){
+                return 1;
+        } else if (n < 0) {
+                return "OOPS";
+        } else if (n > 1) {
+                return (fibonacci (n-1) + fibonacci (n-2));
+        }
+     }
+            
 
-};
+    // if n > 1, then fn = fn-1 + fn-2
+
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people){
+    const currentYear = new Date().getFullYear();
+    const oldestPerson = people.reduce((winner, person) => {
+        if (((person.yearOfDeath || currentYear) - person.yearOfBirth) > ((winner.yearOfDeath || currentYear) - winner.yearOfBirth)) {
+            return person;
+        } else if (((person.yearOfDeath || currentYear) - person.yearOfBirth) <= ((winner.yearOfDeath || currentYear) - winner.yearOfBirth)) {
+            return winner;
+        }
+    });
 
-};
+    return oldestPerson;
+}
 
-// Do not edit below this line
+
+
+
+
 module.exports = findTheOldest;
